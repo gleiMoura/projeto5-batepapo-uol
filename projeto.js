2eb9d93a205a98db159e91b0b63073a5007587e6
Gleison Moura <gle.bar.mou@gmail.com>
2022-02-13 12:35:50 -0300

    put the function thar load the new messages

--- projeto.js
+++ projeto.js
@@ -2,13 +2,9 @@ let login_screen = document.querySelector(".login_screen");
 let appear_after_login = document.querySelector(".appear_after_login");
 let input = document.querySelector("input");
 let login_name = input.value;
-
-function getMesseges(){
-    request = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-    request.then(function(answer){
-        console.log(answer.data)
-    })
-}
+let section = document.querySelector("section");
+let old_data = [];
+let store_data = [];
 
 function takeName(){
     list_name = [];
@@ -26,6 +22,9 @@ function doLogin(answer){
         login_screen.classList.add("hide");
         appear_after_login.classList.remove("hide");
     }
+
+   putMessages();
+   setInterval(putNewMessages, 3000)
 }
 
 function chooseOtherName(answer){
@@ -38,7 +37,6 @@ function continueOn(){
     }
     let itsOn = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", data_person);
     itsOn.then(function (){
-        console.log("tudo certo!")
     })
   
 }
@@ -52,8 +50,74 @@ function clickEnter(){
         })
 }
 
+function putMessages(){
+    section.innerHTML = "";
+
+    request = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    request.then(getMessages);
+
+    function getMessages(answer){
+    let new_data = answer.data;
+    printData(new_data);
+    }
+}
+
+function printData(list){
+    for(let i = 0; i < list.length; i++){
+        if(list[i].type === "status"){
+            section.innerHTML += `
+            <div class="main_message ${list[i].type}">(${list[i].time}) <b>${list[i].from}</b> ${list[i].text}</div>
+        `
+        }else if(list[i].type === "message"){
+            section.innerHTML += `
+            <div class="main_message ${list[i].type}">(${list[i].time}) <b>${list[i].from}</b> para <b>${list[i].to}</b>: ${list[i].text}</div>
+        `
+        }else if(list[i].type === "private_message"){
+            section.innerHTML += `
+            <div class="main_message ${list[i].type}">(${list[i].time}) <b>${list[i].from}</b> reservadamente para <b>${list[i].to}</b>: ${list[i].text}</div>
+        `
+        }  
+    }
+}
+
+function putNewMessages(){
+    request = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    request.then(getMessages);
+
+    function getMessages(answer){
+    let new_data = answer.data;
+   
+    const arr3 = new_data.filter( x => { 
+        return JSON.stringify(old_data).indexOf(JSON.stringify(x)) < 0;
+    });
+    
+    printNewData(arr3);
+
+    old_data = new_data;
+        }
+}
+
+function printNewData(list){
+    for(let i = 0; i < list.length; i++){
+        if(list[i].type === "status"){
+            section.innerHTML += `
+            <div class="main_message ${list[i].type} new>(${list[i].time}) <b>${list[i].from}</b> ${list[i].text}</div>
+        `
+        }else if(list[i].type === "message"){
+            section.innerHTML += `
+            <div class="main_message ${list[i].type} new">(${list[i].time}) <b>${list[i].from}</b> para <b>${list[i].to}</b>: ${list[i].text}</div>
+        `
+        }else if(list[i].type === "private_message"){
+            section.innerHTML += `
+            <div class="main_message ${list[i].type} new">(${list[i].time}) <b>${list[i].from}</b> reservadamente para <b>${list[i].to}</b>: ${list[i].text}</div>
+        `
+        }  
+    }
+}
+
+
 clickEnter();
 setInterval(continueOn, 5000);
-getMesseges();
-
 /*0: {from: 'umNomeAleatórioAí', to: 'Todos', text: 'sai da sala...', type: 'status', time: '04:24:15'}*/
+
+/** */
